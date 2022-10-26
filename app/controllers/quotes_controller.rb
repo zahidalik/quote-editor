331206a777a1b6e6f880a97c58ee8f913be575f3
Zahid Ali Khan <34861896+zahidalik@users.noreply.github.com>
2022-10-26 21:44:40 +0300

    Seed the database with changes in controller and start developing css

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -18,7 +18,7 @@ class QuotesController < ApplicationController
     if @quote.save
       redirect_to quotes_path, notice: "Quote was successfully created."
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
